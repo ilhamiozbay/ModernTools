7e3e96198035586a882c55321b759132b8214027
İlhami ÖZBAY <ilhami.ozbay@gmail.com>
2021-10-31 17:43:08 +0300

    data layer completed

--- back/web-api/ModernTools/DAL/PartialEntities/ModernToolsContext.cs
+++ back/web-api/ModernTools/DAL/PartialEntities/ModernToolsContext.cs
@@ -1,22 +1,54 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DAL.Entities.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace DAL.PartialEntities
 {
-    public class ModernToolsContext
+    public class ModernToolsContext: ModernToolsBaseContext
     {
         public ModernToolsContext()
         {
 
         }
 
-        //public ModernToolsContext(DbContextOptions<> options)
-        //    :base(options)
-        //{
+        public ModernToolsContext(DbContextOptions<ModernToolsBaseContext> options)
+            : base(options)
+        {
 
-        //}
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+#if DEBUG
+            base.OnConfiguring(optionsBuilder);
+            base.OnConfiguring(optionsBuilder
+                .UseLoggerFactory(ModernToolsLoggerFactory));
+#endif
+#if RELEASE
+            base.OnConfiguring(optionsBuilder);
+#endif
+        }
+
+        /* 
+            Yazdığımız entity`lerin log'unun yazılması. 
+            Linq `ların debug moddayken sql execution plan scriptini görmemizi sağlıyor. 
+        */
+        public static readonly ILoggerFactory ModernToolsLoggerFactory 
+            = LoggerFactory.Create(builder =>
+            {
+                builder.AddFilter((category, level) =>
+                    category ==
+                    DbLoggerCategory.Database.Command.Name
+                    && level == LogLevel.Information)
+                .AddDebug();
+            });
 
     }
 }
